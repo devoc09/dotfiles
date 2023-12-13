af6ad5bd80751436aaa80be87644ff3adbbd7f75
Takumi Katase <develop.devoc@gmail.com>
2023-12-13 17:06:14 +0900

    update

--- nvim/lua/plugins/indent.lua
+++ nvim/lua/plugins/indent.lua
@@ -1,7 +1,9 @@
+-- default disabled
 return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     opts = {},
+    enabled = false,
     config = function()
         require("ibl").setup()
     end

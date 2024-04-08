4cff5b01a23cf04ade283aab65b56cdec2a3bfdf
Takumi Katase <develop.devoc@gmail.com>
2024-04-08 19:30:57 +0900

    remove comment plugin in neovim.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,14 +4,6 @@ return {
 	build = ":TSUpdate",
 	dependencies = {
         { "RRethy/nvim-treesitter-endwise" },
-        {
-            "JoosepAlviste/nvim-ts-context-commentstring",
-            config  = function()
-                require("ts_context_commentstring").setup {
-                    enable_autocmd = false,
-                }
-            end
-        },
 	},
 	config = function()
 		local configs = require("nvim-treesitter.configs")

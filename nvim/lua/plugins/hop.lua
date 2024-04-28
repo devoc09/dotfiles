63439c57ea539f00ab408b50f1e0012c9553858a
Takumi Katase <develop.devoc@gmail.com>
2024-04-28 18:46:21 +0900

    change keymaps of hop.nvim

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -4,9 +4,9 @@ return {
         "nvim-lua/plenary.nvim",
     },
     keys = {
-        { "ff", function() require("hop").hint_char1() end },
-        { "fj", function() require("hop").hint_lines_skip_whitespace() end },
-        { "fj", function() require("hop").hint_words() end },
+        { "f", function() require("hop").hint_char1() end },
+        -- { "fj", function() require("hop").hint_lines_skip_whitespace() end },
+        -- { "fj", function() require("hop").hint_words() end },
     },
     config = function()
         require("hop").setup({ multi_windows = true })

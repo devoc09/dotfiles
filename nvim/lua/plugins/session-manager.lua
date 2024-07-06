bb2e4ab3fa2d4bd2f5dba6475a9b2b026a83c200
Takumi Katase <develop.devoc@gmail.com>
2024-07-06 23:29:07 +0900

    add neovim plugin 'session-manager.nvim'

---
+++ nvim/lua/plugins/session-manager.lua
@@ -0,0 +1,12 @@
+return {
+  'devoc09/session-manager.nvim',
+  event = 'BufReadPre',
+  opts = {}
+}
+
+-- for local development
+-- return {
+--   dir = '/home/kumico/go/src/github.com/devoc09/session-manager.nvim',
+--   event = 'BufReadPre',
+--   opts = {}
+-- }

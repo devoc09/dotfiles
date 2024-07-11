5bd5b9cf6fb0139ba111066d1518e7fcb0d1f593
Takumi Katase <develop.devoc@gmail.com>
2024-07-11 22:20:26 +0900

    format hop & lsp-config lua file

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -1,14 +1,12 @@
 return {
-    "phaazon/hop.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    keys = {
-        { "f", function() require("hop").hint_char1() end },
-        -- { "fj", function() require("hop").hint_lines_skip_whitespace() end },
-        -- { "fj", function() require("hop").hint_words() end },
-    },
-    config = function()
-        require("hop").setup({ multi_windows = true })
-    end,
+  "phaazon/hop.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  keys = {
+    { "f", function() require("hop").hint_char1() end },
+  },
+  opts = {
+    multi_windows = true,
+  },
 }
--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,56 +1,59 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-buffer",
-	},
-	config = function()
-		local cmp = require("cmp")
-		cmp.setup({
-			preselect = {
-				none = true,
-			},
-			mapping = {
-				["<C-n>"] = cmp.mapping.select_next_item(),
-				["<C-p>"] = cmp.mapping.select_prev_item(),
-			},
-			sources = {
-				{
-					name = "nvim_lsp",
-					option = {
-						get_bufnrs = function()
-							return vim.api.nvim_list_bufs()
-						end
-					},
-				},
-				{
-					name = "buffer",
-					option = {
-						get_bufnrs = function()
-							local bufs = {}
-							for _, win in ipairs(vim.api.nvim_list_wins()) do
-								bufs[vim.api.nvim_win_get_buf(win)] = true
-							end
-							return vim.tbl_keys(bufs)
-						end
-					},
-				},
-				{ name = "path" },
-			},
-		})
-		cmp.setup.cmdline("/", {
-			sources = {
-				{ name = "buffer" }
-			}
-		})
-		cmp.setup.cmdline(":", {
-			sources = cmp.config.sources({
-				{ name = "path" }
-			}, {
-				{ name = "cmdline" }
-			})
-		})
-	end,
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-buffer",
+  },
+  opts = function()
+    local cmp = require("cmp")
+    cmp.setup({
+      view = {
+        entries = 'custom'
+      },
+      preselect = {
+        none = true,
+      },
+      mapping = {
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+      },
+      sources = {
+        {
+          name = "nvim_lsp",
+          option = {
+            get_bufnrs = function()
+              return vim.api.nvim_list_bufs()
+            end
+          },
+        },
+        {
+          name = "buffer",
+          option = {
+            get_bufnrs = function()
+              local bufs = {}
+              for _, win in ipairs(vim.api.nvim_list_wins()) do
+                bufs[vim.api.nvim_win_get_buf(win)] = true
+              end
+              return vim.tbl_keys(bufs)
+            end
+          },
+        },
+        { name = "path" },
+      },
+    })
+    cmp.setup.cmdline("/", {
+      sources = {
+        { name = "buffer" }
+      }
+    })
+    cmp.setup.cmdline(":", {
+      sources = cmp.config.sources({
+        { name = "path" }
+      }, {
+        { name = "cmdline" }
+      })
+    })
+  end,
 }

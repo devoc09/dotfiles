75f500e40c27631c813fcb59205196625b1d1b7e
Takumi Katase <develop.devoc@gmail.com>
2024-07-11 18:32:56 +0900

    config scrolloff settings of neovim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -9,22 +9,23 @@ vim.o.number = false
 vim.o.cursorline = true
 -- vim.o.clipboard = 'unnamed'
 vim.o.belloff = 'all'
+vim.o.scrolloff = 8
 
 -- enable set number
 vim.cmd('set number')
 
 -- Tab and Indentation
-vim.o.autoindent = true -- Continue indent of the previous line on newline
+vim.o.autoindent = true  -- Continue indent of the previous line on newline
 vim.o.smartindent = true -- Auto-insert indentation according to syntax
-vim.o.shiftwidth = 4 -- Indentation amount for < and > commands
-vim.o.tabstop = 4 -- Number of spaces that a tab in the file counts for
-vim.o.expandtab = true -- Convert tabs to spaces
+vim.o.shiftwidth = 4     -- Indentation amount for < and > commands
+vim.o.tabstop = 4        -- Number of spaces that a tab in the file counts for
+vim.o.expandtab = true   -- Convert tabs to spaces
 
 -- String search settings
-vim.o.incsearch = true -- Incremental search, searching as you type
+vim.o.incsearch = true  -- Incremental search, searching as you type
 vim.o.ignorecase = true -- Case insensitive searching
-vim.o.smartcase = true -- Case sensitive if search pattern contains uppercase
-vim.o.hlsearch = true -- Highlight search results
+vim.o.smartcase = true  -- Case sensitive if search pattern contains uppercase
+vim.o.hlsearch = true   -- Highlight search results
 
 -- window split config
 vim.o.splitright = true
@@ -35,5 +36,5 @@ vim.o.laststatus = 3
 
 -- guicolors
 if os.getenv("COLORTERM") ~= nil then
-    vim.o.termguicolors = true
+  vim.o.termguicolors = true
 end

1210d237a93cc5be29c323b2dd0500ac46b321d7
Takumi Katase <50615605+devoc09@users.noreply.github.com>
2024-10-22 19:26:19 +0900

    Update .wezterm.lua

--- terminals/wezterm/.wezterm.lua
+++ terminals/wezterm/.wezterm.lua
@@ -3,49 +3,24 @@ local wezterm = require 'wezterm'
 local config = {}
 
 if wezterm.confing_builder then
-  config = wezterm.confing_builder()
+    config = wezterm.confing_builder()
 end
 
-config.color_scheme = 'default'
--- `SF Mono` is apple font
--- config.font = wezterm.font('SF Mono', { weight = 'Bold', italic = false })
-config.font = wezterm.font('UDEV Gothic 35NFLG', { italic = false })
-config.font_size = 11
+config.font = wezterm.font('UDEV Gothic NFLG', { italic = false })
+config.font_size = 14
 
 -- startup size
-config.initial_rows = 80
-config.initial_cols = 230
+config.initial_rows = 50
+config.initial_cols = 170
+
+config.cursor_blink_rate = 0
 
 config.color_schemes = {
   ['lflops'] = {
-    -- [0]  = "#373b41" /* black   */
-    -- [1]  = "#ff5682" /* red     */
-    -- [2] = "#7fd06e" /* green   */
-    -- [3] = "#ffd982" /* yellow  */
-    -- [4] = "#00dbea" /* blue    */
-    -- [5] = "#9b8fcb" /* magenta */
-    -- [6] = "#f771be" /* cyan    */
-    -- [7] = "#d2cdf4" /* white   */
-    -- [8] = "#485890" /* black   */
-    -- [9] = "#c24163" /* red     */
-    -- [10] = "#6eb35f" /* green   */
-    -- [11] = "#c3a664" /* yellow  */
-    -- [12] = "#00919b" /* blue    */
-    -- [13] = "#9372af" /* magenta */
-    -- [14] = "#ca5c9c" /* cyan    */
-    -- [15] = "#b8b8ba" /* white   */
-    -- [256] = "#1d1f21", /* background */
-    -- [257] = "#d2cdf4", /* foreground */
-
     foreground = '#d2cdf4',
-    background = '#333333',
-    cursor_bg = 'orange',
-    cursor_fg = 'orange',
-    cursor_border = 'orange',
-    selection_fg = 'black',
-    selection_bg = '#fffacd',
-    scrollbar_thumb = '#222222',
-    split = '#444444',
+    background = '#0c172a',
+    cursor_fg = '#0c172a',
+    cursor_bg = '#d2cdf4',
     ansi = {
       '#333333',
       '#ff5682',
@@ -69,19 +44,50 @@ config.color_schemes = {
   },
 }
 
-config.colors = {
-  cursor_bg = 'orange',
-  cursor_border = 'orange',
-  compose_cursor = 'orange',
+config.window_frame = {
+  inactive_titlebar_bg = 'none',
+  active_titlebar_bg = 'none',
 }
 
-config.color_scheme = "tender (base16)"
+config.window_background_gradient = {
+  colors = { '#0c172a' },
+}
+
+config.show_new_tab_button_in_tab_bar = false
+config.show_close_tab_button_in_tabs = false
+
+config.colors = {
+  tab_bar = {
+    inactive_tab_edge = 'none',
+  },
+}
+
+ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+   local background = "#242b3a"
+   local foreground = "#d2cdf4"
+
+   if tab.is_active then
+     background = "#485890"
+     foreground = "#d2cdf4"
+   end
+
+   local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+
+   return {
+     { Background = { Color = background } },
+     { Foreground = { Color = foreground } },
+     { Text = title },
+   }
+ end)
+
+config.color_scheme = "lflops"
 
 config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = 'RESIZE'
 
 config.inactive_pane_hsb = {
-  saturation = 0.9,
-  brightness = 0.5,
+    saturation = 0.9,
+    brightness = 0.5,
 }
 
 -- scrollbar
@@ -91,49 +97,51 @@ config.scrollback_lines = 10000
 -- keymaps
 act = wezterm.action
 config.keys = {
-  {
-    key = 'd',
-    mods = 'CMD',
-    action = act.SplitVertical { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'd',
-    mods = 'CMD|SHIFT',
-    action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = '[',
-    mods = 'CMD',
-    action = act.ActivatePaneDirection 'Next',
-  },
-  {
-    key = ']',
-    mods = 'CMD',
-    action = act.ActivatePaneDirection 'Prev',
-  },
-  {
-    key = 'j',
-    mods = 'CMD',
-    action = act.QuickSelect,
-  },
-  {
-    key = 'w',
-    mods = 'CMD',
-    action = wezterm.action.CloseCurrentPane { confirm = true },
-  },
-
+    {
+        key = 'd',
+        mods = 'CMD',
+        action = act.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+    {
+        key = 'd',
+        mods = 'CMD|SHIFT',
+        action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+    {
+        key = '[',
+        mods = 'CMD',
+        action = act.ActivatePaneDirection 'Next',
+    },
+    {
+        key = ']',
+        mods = 'CMD',
+        action = act.ActivatePaneDirection 'Prev',
+    },
+    {
+        key = 'j',
+        mods = 'CMD',
+        action = act.QuickSelect,
+    },
+    {
+        key = 'w',
+        mods = 'CMD',
+        action = wezterm.action.CloseCurrentPane { confirm = true },
+    },
 }
 
 -- SSH Domains
 local ssh_domains = {}
 for host, config in pairs(wezterm.enumerate_ssh_hosts()) do
-  table.insert(ssh_domains, {
-    name = host,
-    remote_address = config["hostname"],
-    username = config["user"],
-    ssh_option = { identityfile = config["identityfile"] },
-  })
+    table.insert(ssh_domains, {
+        name = host,
+        remote_address = config["hostname"],
+        username = config["user"],
+        ssh_option = { identityfile = config["identityfile"] },
+    })
 end
 config.ssh_domains = ssh_domains
+
+-- Japanese input
+config.use_ime = true
 
 return config

dfd2a6679c4d5b4a0df18adfcacc5f02a5d40acb
Takumi Katase <50615605+devoc09@users.noreply.github.com>
2025-01-16 00:56:55 +0900

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,32 +1,32 @@
+#------------------------------------------------------
+# prompt
+#------------------------------------------------------
 # disable welcome message
 set -g -x fish_greeting
 
-
-function fish_prompt --description 'define iteractive shell prompt'
-    # insert new line befor prompt
+function fish_prompt --description 'define iteractive shell prompt left'
+    # Insert new line before prompt
     echo
 
     # username & hostname
     set -l short_hostname (hostname | cut -d . -f 1)
-    set_color normal
-    echo -n (whoami); echo -n '@'; echo -n $short_hostname; echo -n ':'
+    echo -n (whoami)"@"$short_hostname":"
 
     # current directory
-    set_color blue
-    echo -n (prompt_pwd)
+    echo -en "\e[34m"(prompt_pwd)"\e[0m"
 
     # prompt synbol
-    set_color normal
+    #set_color normal
     set -l git_branch (__fish_git_prompt | string trim -c ' ')
-    echo $git_branch; echo -n '$ '
+    echo -e "$gitbranch\n"'$ '
 end
 
 function fish_right_prompt --description 'define interactive shell prompt right'
     # exit code
     set -l last_status $status
     if test $last_status -ne 0
-        set_color red
-        echo -n ' '$last_status
+        #set_color red
+        echo -ne "\e[31m$last_status\e[0m"
     end
 end
 
@@ -41,5 +41,8 @@ function fish_user_key_bindings
     bind \x1d fzf_select_ghq_list
 end
 
+#------------------------------------------------------
+# aliases
+#------------------------------------------------------
 alias ls='ls --color=always'
 funcsave ls
